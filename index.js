4a93a1247a8fecbb3af54b5c8deb1ecd837c6231
netovieira01_ <netinhovieira1801@gmail.com>
2021-10-29 11:10:26 -0300

    Null e Undefined

--- index.js
+++ index.js
@@ -1,25 +1,17 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
-const cliente1 = new Cliente();
-cliente1.nome = 'Manoel';
-cliente1.cpf = 9433726405;
-
 const contaCorrenteManoel = new ContaCorrente();
+contaCorrenteManoel.cliente = new Cliente();
 contaCorrenteManoel.agencia = 2049;
-contaCorrenteManoel.cliente = cliente1;
-
-contaCorrenteManoel.depositar(500);
-
-const cliente2 = new Cliente();
-cliente2.nome = 'Messia';
-cliente2.cpf = 12840568426;
+contaCorrenteManoel.cliente.nome = 'Manoel';
+contaCorrenteManoel.cliente.cpf = 59433726405;
 
 const contaCorrenteMessia = new ContaCorrente();
+contaCorrenteMessia.cliente = new Cliente();
 contaCorrenteMessia.agencia = 2049;
-contaCorrenteMessia.cliente = cliente2;
-
-contaCorrenteManoel.transferir(200, contaCorrenteMessia);
+contaCorrenteMessia.cliente.nome = 'Messia';
+contaCorrenteMessia.cliente.cpf = 17563586540;
 
 console.log(contaCorrenteManoel);
 console.log(contaCorrenteMessia);
