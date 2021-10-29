4a09b41a95b016011bf2e1b3d639f27862086ca6
netovieira01_ <netinhovieira1801@gmail.com>
2021-10-29 08:20:05 -0300

    Criando Método Sacar

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 class Cliente {
     nome;
     cpf;
+}
+
+class ContaCorrente{
     agencia;
     saldo;
+
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = 'Manoel';
 cliente1.cpf = 09433726405;
-cliente1.agencia = 2049;
-cliente1.saldo = 0;
 
+const contaCorrenteManoel = new ContaCorrente();
+contaCorrenteManoel.agencia = 2049;
+contaCorrenteManoel.saldo = 100;
+console.log(contaCorrenteManoel.saldo);
+contaCorrenteManoel.sacar(75);
+console.log(contaCorrenteManoel.saldo);
+
+const cliente2 = new Cliente();
 cliente2.nome = 'Messia';
 cliente2.cpf = 12840568426;
-cliente2.agencia = 2049;
-cliente2.saldo = 0;
 
 console.log(cliente1, cliente2);
 
